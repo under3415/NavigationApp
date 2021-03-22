333c7990d72e89e7815ac794846e898736dc0203
kawaka <dejan@outlook.co.nz>
2021-03-22 22:00:47 +1300

    created

---
+++ MainPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace NavigationApp
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private async void Button_Click(object sender, RoutedEventArgs e)
+        {
+            ContentDialog dialog = new ContentDialog
+            {
+                Content = "Press ALT + C ",
+                Title = "Dialog",
+                PrimaryButtonText = "OK"
+            };
+
+
+
+            _ = await dialog.ShowAsync();
+        }
+    }
+}
---
+++ NavigationRoot.xaml.cs
@@ -0,0 +1,31 @@
+﻿
+using Windows.UI;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Media;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace NavigationApp
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class NavigationRoot : Page
+    {
+        public NavigationRoot()
+        {
+            this.InitializeComponent();
+        }
+
+        private void Page_Loaded(object sender, RoutedEventArgs e)
+        {
+            RootContentFrame.Navigate(typeof(MainPage));
+        }
+
+        private void Button1_Click(object sender, RoutedEventArgs e)
+        {
+            this.Background = new SolidColorBrush(Color.FromArgb(255, 48, 179, 221));
+        }
+    }
+}
