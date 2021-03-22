f84b5b07f9679e8fd06d060710a8672e33b50569
kawaka <dejan@outlook.co.nz>
2021-03-23 11:06:47 +1300

    second button

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using System;
+using Windows.UI;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Media;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
@@ -11,6 +13,8 @@ namespace NavigationApp
     /// </summary>
     public sealed partial class MainPage : Page
     {
+        private Random rnd = new Random();
+
         public MainPage()
         {
             this.InitializeComponent();
@@ -20,7 +24,7 @@ namespace NavigationApp
         {
             ContentDialog dialog = new ContentDialog
             {
-                Content = "Press ALT, then C ",
+                Content = "Press ALT, then C or V",
                 Title = "Non Modal Dialog",
                 PrimaryButtonText = "OK"
             };
@@ -28,6 +32,11 @@ namespace NavigationApp
 
 
             _ = await dialog.ShowAsync();
+        }
+
+        private void Button3_Click(object sender, RoutedEventArgs e)
+        {
+            this.Background = new SolidColorBrush(Color.FromArgb((byte)rnd.Next(256), (byte)rnd.Next(256), (byte)rnd.Next(256), (byte)rnd.Next(256)));
         }
     }
 }
